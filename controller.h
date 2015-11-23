9c866750747e1b016a9a7ef97da8adb82c09858d
Javin-Ambridge <javin.ambridge@gmail.com>
2015-11-23 15:41:52 -0500

    setup player.h

--- controller.h
+++ controller.h
@@ -4,7 +4,7 @@
 #include "player.h"
 
 class Controller {
-	void startFromSave(string fileName);
+	void startFromSave(std::string fileName);
 	int numberOfPlayers;
 	public:
 
--- player.h
+++ player.h
@@ -1,5 +1,41 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
 
+class Tile;
+
+class Player{
+  string name;
+  string piece;
+  int money;
+  int netWorth;
+  bool rollUpCup;
+  Tile * pos;
+  Tile * properties 
+  public:
+
+    //CTOR
+    Player(std::string name, std::string piece);
+    //DTOR
+    ~Player();
+
+    //getters
+    string getName();
+    string getChar();
+    int getMoney();
+    int getNetWorth();
+    Tile & getPos();
+
+    //Player moves
+    void roll();
+    void improve(Tile * t);
+    void mortgage(Tile * t);
+    void unmortage(Tile * t);
+    bool hasProperty(Tile & t);
+    void removeProperty(Tile & t);
+    void addProperty(Tile & t);
+
+
+};
 
 #endif
