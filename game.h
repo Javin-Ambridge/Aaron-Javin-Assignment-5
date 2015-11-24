8b5be2ca7c94630ad402f6eac003c6fb3032bdba
Javin-Ambridge <javin.ambridge@gmail.com>
2015-11-23 20:03:22 -0500

    fixed up errors

--- game.h
+++ game.h
@@ -1,8 +1,32 @@
 #ifndef GAME_H
 #define GAME_H
+#include <iostream>
+#include <string>
+#include "player.h"
+#include "tile.h"
+
 
 class Game {
-
+		Player *players[8];
+		int numberOfPlayers;
+		int currentRollupCups;
+		Tile board[40];
+		int numPlayers;
+		bool active;
+	public:
+		void save();
+		void load();
+		bool isActive();
+		bool isPieceUsed(std::string piece);
+		bool isTileOwned(Tile &t);
+		int getNumberOfPlayers();
+		void setNumberOfPlayers(int num);
+		void addPlayer(std::string name, std::string piece);
+		Tile getPosition(int playerIndex);
+		void doMove(int playerIndex);
+		bool canGetRollup();
+		void wonRollup();
+		void usedRollup();
 };
 
 #endif
